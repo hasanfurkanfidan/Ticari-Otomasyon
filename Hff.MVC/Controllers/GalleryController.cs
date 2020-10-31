68f49628053996e4b49b67ab256f50c338bdef56
hasanfurkanfidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2020-10-31 19:11:34 +0300

    Gallery Module Added

--- Hff.MVC/Controllers/GalleryController.cs
+++ Hff.MVC/Controllers/GalleryController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Hff.Business.Abstract;
+using Hff.MVC.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,23 @@ namespace Hff.MVC.Controllers
 {
     public class GalleryController : Controller
     {
+        private readonly IProductService _productService;
+        public GalleryController(IProductService productService)
+        {
+            _productService = productService;
+        }
         // GET: Gallery
         public ActionResult Index()
         {
-            return View();
+            var models =new List<GalleryViewModel>();
+            var products = _productService.GetList();
+            foreach (var product in products)
+            {
+                var model = new GalleryViewModel();
+                model.ImageUrls = product.Picture;
+                models.Add(model);
+            }
+            return View(models);
         }
     }
 }
